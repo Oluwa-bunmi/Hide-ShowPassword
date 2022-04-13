6063a32bbde3dcf51c8809c4e61dd8a7786d6a6f
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-04-13 17:15:01 +0100

    still working

---
+++ main.js
@@ -0,0 +1,16 @@
+let eyes = document.querySelectorAll('i')
+eyes.forEach(eye => eye.addEventListener('click', (event) => {
+    let inputs, inputtype, input
+    inputs = document.querySelectorAll('input')
+    input = event.target.closest('.form-group').querySelector('input')
+    inputtype = input.getAttribute('type')
+    if (inputtype === 'password') {
+        input.setAttribute("type", "text")
+        // document.getElementsByTagName('i')[3]
+    } else if (inputtype === 'text') { input.setAttribute("type", "password") } else { input.setAttribute("type", "text") }
+    console.log(inputtype)
+    
+}))
+
+
+
