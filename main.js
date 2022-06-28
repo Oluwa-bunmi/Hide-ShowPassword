407d1b105507373f8af57f2f5043128d3e8b5f8a
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-28 16:24:07 +0100

    js working fully

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-let eyes = document.querySelectorAll('i')
-eyes.forEach(eye => eye.addEventListener('click', (event) => {
-    let inputs, inputtype, input
-    inputs = document.querySelectorAll('input')
-    input = event.target.closest('.form-group').querySelector('input')
-    inputtype = input.getAttribute('type')
-    if (inputtype === 'password') {
-        input.setAttribute("type", "text")
-        // document.getElementsByTagName('i')[3]
-    } else if (inputtype === 'text') { input.setAttribute("type", "password") } else { input.setAttribute("type", "text") }
-    console.log(inputtype)
-    
-}))
-
-
-
+let eyes = document.querySelectorAll(".fa-eye");
+let firstIcon = document.getElementById("closed");
+let secondIcon = document.getElementById("close");
+firstIcon.onclick = function () {
+  firstIcon.classList.toggle("fa-eye-slash");
+};
+secondIcon.onclick = function () {
+  secondIcon.classList.toggle("fa-eye-slash");
+};
+eyes.forEach((eye) =>
+  eye.addEventListener("click", (event) => {
+    let inputtype, input;
+    input = event.target.closest(".form-group").querySelector("input");
+    inputtype = input.getAttribute("type");
+    if (inputtype === "password") {
+      input.setAttribute("type", "text");
+    } else if (inputtype === "text") {
+      input.setAttribute("type", "password");
+    } else {
+      input.setAttribute("type", "text");
+    }
+  })
+);
